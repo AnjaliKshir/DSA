8e6497ae40143e1097a5aad2448b9e8e6f75e1a3
AnjaliKshir <anjuksagar2003@gmail.com>
2023-10-16 02:32:45 +0530

    Number Square Pattern

---
+++ Patterns/pattern_1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cin>>n;
+     int i = n;
+    
+     while(i>0)
+     {
+         int j = n;
+         while(j>0)
+         {
+             cout<<j;
+             j=j-1;
+         }
+         cout<<endl;
+         i=i-1;
+        
+        
+     }
+    
+    
+}
+
+
