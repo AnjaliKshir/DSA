02c8722eaf5148697823bb698560b39cb57749f9
AnjaliKshir <anjuksagar2003@gmail.com>
2023-11-03 23:08:16 +0530

    Alphabet_Square_Pattern_1

---
+++ Patterns/pattern_5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cin>>n;
+    int i = 1;
+    int k;
+    
+     while(i<=n)
+     {
+         int j = 1;
+         k=i;
+         while(j<=i)
+         {
+             cout<<k--;
+             j=j+1;
+         }
+         cout<<endl;
+         i=i+1;
+     }   
+}
